d42fbb79c6279107aabcca2bb66cb2e186d2f7fc
Mathankumar <46513026+MathankumarSelvaraj@users.noreply.github.com>
2024-05-17 17:03:39 +0530

    Update home.py

--- home.py
+++ home.py
@@ -4,7 +4,7 @@ import pandas as pd
 from streamlit_option_menu import option_menu
 from numerize.numerize import numerize
 import time
-from streamlit_extras.metric_cards import style_metric_cards
+#from streamlit_extras.metric_cards import style_metric_cards
 #st.set_option('deprecation.showPyplotGlobalUse', False)
 import plotly.graph_objs as go
 import datetime
