a46de0f7b4f3e3ab6b509e14003a6f9e9cf49c5f
Mathankumar <mathankumarselvarajreddy@gmail.com>
2024-05-30 16:46:28 +0530

    post

--- pages/main.py
+++ pages/main.py
@@ -5,14 +5,17 @@ import pandas as pd #data manipulation
 #page setting
 st.set_page_config(page_title="AIR 7 SEAS Air Export Volume Dashboard", page_icon=":bar_chart:", layout="wide")
 
-st.title("AIR 7 SEAS Air Export Volume Dashboard")
+#st.title("AIR 7 SEAS Air Export Volume Dashboard")
 #reading data
-df = pd.read_csv(r'D:/airExportVolumeDashboard/datasets/airExportVolume2023.csv')
+df = pd.read_csv(r'datasets/airExportVolume2023.csv')
 
 #varibale defining
-
-df['Crea'] = pd.to_datetime(df['Crea'])
-distintYear = df['Crea'].dt.year.unique()
+df['createdDate'] = pd.to_datetime(df['createdDate'])
+        # df['year'] = df['createdDate'].dt.year
+        # df['month'] = df['createdDate'].dt.month
+        # df['date'] = df['createdDate'].dt.date
+# df['Crea'] = pd.to_datetime(df['Crea'])
+distintYear = df['createdDate'].dt.year.unique()
 destinationCountry=df["Destination country"].unique()
 
 #--SIDEBAR FOR FILTER
@@ -22,31 +25,31 @@ dtCountry = st.sidebar.multiselect(
     options=destinationCountry
 )
 
-year = st.sidebar.selectbox(
+year = st.sidebar.multiselect(
     "select the year:",
-    options = distintYear
+    options = distintYear,default=2024
 )
 
-df_selection = df.query ("`Destination country` == @dtCountry")
+# df_selection = df.query ("`Destination country` == @dtCountry")
 
 groupbycounrty = pd.DataFrame(df.groupby("Destination country").sum("Considerable Charging Unit"))
 
 countrydata = groupbycounrty.sort_values("Considerable Charging Unit",ascending=False)
 
-#Scorecard
-if len(dtCountry)>0:
-    sumOfVolume= df_selection["Considerable Charging Unit"].sum()
-else:
-    sumOfVolume= df["Considerable Charging Unit"].sum()
+# #Scorecard
+# if len(dtCountry)>0:
+#     sumOfVolume= df_selection["Considerable Charging Unit"].sum()
+# else:
+#     sumOfVolume= df["Considerable Charging Unit"].sum()
 
-#countrywise table
+# #countrywise table
 
 if len(dtCountry)>0:
     volumebycountry= countrydata
 else:
-    volumebycountry= countrydata.query("`Destination country` == @dtCountry")
+     volumebycountry= countrydata.query("`Destination country` == @dtCountry")
 
 
-#showing scorecard and countrytable
-st.metric(label="Total Volume",value = sumOfVolume,delta=None)
+# #showing scorecard and countrytable
+# st.metric(label="Total Volume",value = sumOfVolume,delta=None)
 st.dataframe(volumebycountry)
--- pages/test.py
+++ pages/test.py
@@ -1,31 +1,31 @@
-import streamlit as st
-from streamlit_keyup import st_keyup # type: ignore
+# import streamlit as st
+# # from streamlit_keyup import st_keyup 
 
-# Sample list of airlines
-airlines = ["American Airlines", "Delta Airlines", "United Airlines", "Southwest Airlines", "JetBlue Airways"]
+# # Sample list of airlines
+# airlines = ["American Airlines", "Delta Airlines", "United Airlines", "Southwest Airlines", "JetBlue Airways"]
 
-# Initialize session state for checkbox states if not already done
-if 'checkbox_states' not in st.session_state:
-    st.session_state.checkbox_states = {air: False for air in airlines}
+# # Initialize session state for checkbox states if not already done
+# if 'checkbox_states' not in st.session_state:
+#     st.session_state.checkbox_states = {air: False for air in airlines}
 
-# Function to filter airlines based on the search query
-def filter_airlines(query, airlines):
-    return [air for air in airlines if query.lower() in air.lower()]
+# # Function to filter airlines based on the search query
+# def filter_airlines(query, airlines):
+#     return [air for air in airlines if query.lower() in air.lower()]
 
-with st.expander(label="Select an Airline"):
-    # Text input for filtering with st_keyup to capture keyup events
-    query = st_keyup("Filter")
+# with st.expander(label="Select an Airline"):
+#     # Text input for filtering with st_keyup to capture keyup events
+#     query = st_keyup("Filter")
     
-    # Filter the list of airlines based on the query
-    filtered_airlines = filter_airlines(query, airlines)
+#     # Filter the list of airlines based on the query
+#     filtered_airlines = filter_airlines(query, airlines)
     
-    # Display checkboxes for the filtered airlines
-    for air in filtered_airlines:
-        st.session_state.checkbox_states[air] = st.checkbox(label=f"{air}", value=st.session_state.checkbox_states[air])
+#     # Display checkboxes for the filtered airlines
+#     for air in filtered_airlines:
+#         st.session_state.checkbox_states[air] = st.checkbox(label=f"{air}", value=st.session_state.checkbox_states[air])
     
-    # Get the list of selected airlines
-    selected_airlines = [air for air, checked in st.session_state.checkbox_states.items() if checked]
+#     # Get the list of selected airlines
+#     selected_airlines = [air for air, checked in st.session_state.checkbox_states.items() if checked]
     
-    # Print the selected airlines
-    st.write("Selected Airlines:", selected_airlines)
+#     # Print the selected airlines
+#     st.write("Selected Airlines:", selected_airlines)
 
